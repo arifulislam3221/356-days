32ba128d3a4ef1ac2400faabe1da555a20d8739c
ArifulIslam3221 <pkmep4670@gmail.com>
2024-02-16 00:11:37 +0600

    add nav and day 1 task

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const translateButton = document.getElementById('translate-btn').addEventListener('click', function(){
+    const sectionOne = document.getElementById('translate-into-bangla');
+    sectionOne.classList.remove('hidden');
+})
